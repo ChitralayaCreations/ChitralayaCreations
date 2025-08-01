16d7eaf38758153305a0a0f34439fdb0acb2f865
ChitralayaCreations <akashchakali674@gmail.com>
2025-08-01 21:35:47 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,21 +50,6 @@ document.addEventListener('visibilitychange',
     });
 
 });
-const cursor = document.querySelector('.custom-cursor');
-
-document.addEventListener('mousemove', (e) => {
-  cursor.style.display = 'block';
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-});
-
-document.addEventListener('mouseleave', () => {
-  cursor.style.display = 'none';
-});
-
-document.addEventListener('mouseenter', () => {
-  cursor.style.display = 'block';
-});
 
 // Counter Animation on Scroll
 let countersAnimated = false;
